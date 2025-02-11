d693d7373474e18bdaa640a44c2255a003f8bcf4
Stanley Lim <stannielim@meta.com>
2025-02-10 19:51:33 -0500

    socket messages

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,31 @@
 const express = require("express");
 const cors = require("cors");
+const socketio = require("socket.io");
 
 const app = express();
 
 app.use(cors());
 
-app.listen(3000);
+// app.listen(...) returns the server that it created
+const server = app.listen(3000);
+
+// socketio requires a server
+// socketio turns your server into a 'socket server'
+const io = socketio(server, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
+const messages = [];
+
+io.on("connection", (socket) => {
+  // when a connection is made to the server, what can you do to send all the messages to the client that was connected?
+  socket.emit("allMessages", messages);
+
+  socket.on("sendMessage", (data) => {
+    messages.push(data);
+
+    io.emit("allMessages", messages);
+  });
+});
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
+
+import "./App.css";
+
+// this sends a 'connection' request to your socket io server
+// you sent a connection request
+// but your backend is not listening to any connection requests yet
+const socket = io("http://localhost:3000");
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState("");
+
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    socket.on("allMessages", (data) => {
+      setMessages(data);
+    });
+  }, []);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+
+    // emit an event to the socket server
+    // the event name
+    // data that you want to pass thru
+    socket.emit("sendMessage", message);
+    setMessage("");
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ul>
+        {messages.map((message, index) => (
+          <li key={index}>{message}</li>
+        ))}
+      </ul>
+
+      <form onSubmit={sendMessage}>
+        <input
+          value={message}
+          onChange={(event) => setMessage(event.target.value)}
+        />
+        <button>Send message</button>
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
